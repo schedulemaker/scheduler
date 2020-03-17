5001534f06a8e8645f2da766a90a529024bb9921
ericgurevich <tug00319@temple.edu>
2020-03-16 21:02:07 -0400

    started scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,83 @@
+NUM_WEEKS = 16
+
+def noConflict(section,schedule):
+    '''/**
+    * @function noConflict - Checks if the given section conflicts with any other sections in the schedule
+    * @param {Section} section - The section to check against
+    * @param {[[string, Section]]} schedule - An array of string-Section pairs representing the current schedule
+    * @return {boolean} - TRUE if the section does not conflict with any other section in the array, FALSE otherwise
+    */'''
+    for sched in schedule:
+        if hasTimeConflict(section, sched):
+            return False
+    return True
+
+def hasTimeConflict(course1, course2):
+    for week in range(0, NUM_WEEKS):
+        for day in range(0,7):
+            for meeting in course1['weeks'][week][day]['meetingTimes'])
+            
+}
+
+
+'''
+    Proposed format
+{
+    crn: number //RANGE key
+	courseName: string //HASH key
+	description: string
+	attributes: string[]
+    weeks: [
+        {
+            days: [
+                {
+                    day: Monday
+                    meetingTimes: [
+                        {
+                            building:
+                            room:
+                            .
+                            .
+                            .
+                            startTime:
+                            endTime:
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+'''
+
+'''
+    Old format
+
+    {
+	crn: number //RANGE key
+	courseName: string //HASH key
+	description: string
+	attributes: string[]
+	meetingTimes: [
+		{
+			startDate: string
+			endDate: string
+			Monday: [
+                      {
+				startTime: number
+				endTime: number
+				building: string
+				room: string
+				instructors: string[]
+				campus: string
+			    }
+			]
+			//Same for other days of the week
+			//Incl. weekends
+		}
+		//Other entries for each week of the semester
+	]
+}'''
 
 '''table = dynamodb.Table('Movies')
 
